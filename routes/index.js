7565f0160c83d8c87a01966ce635c49cacb10f0b
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-11-12 10:03:33 +0100

    Add YouTube API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router  = express.Router();
+const {google} = require('googleapis');
+const youtube = google.youtube({
+   version: 'v3',
+   auth: process.env.YOUTUBE_API_KEY
+});
+
+
+
+
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+        res.render('index');
+      });
+
+router.get('/query', (req, res, next) => {
+  youtube.search.list({
+    maxResults: "5",
+      part: 'snippet',
+      q: 'Sharks',
+      type: "video",
+      videoEmbeddable: "true"
+    }, function (err, data) {
+      if (err) {
+        console.error('Error: ' + err);
+      }
+      if (data) {
+        console.log(data.data.items[0])
+        res.redirect('/');
+      }
+    });
 });
 
 router.get("/main" , (req,res,next) => {
