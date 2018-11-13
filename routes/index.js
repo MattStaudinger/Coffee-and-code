403982e19bfd85a117c6318aefc93cd98b30ef66
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-11-13 20:11:08 +0100

    Add cafe-page and new features

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const router  = express.Router();
 const uploadCloud = require('../config/cloudinary.js');
 const Cafe = require('../models/Cafe');
 
+function ensureAuthenticated(req, res, next) {
+  if (req.user) {
+    return next();
+  } else {
+    res.redirect('/auth/login')
+  }
+}
+
 router.get('/', (req, res, next) => {
         res.render('index');
       });
@@ -24,8 +32,14 @@ router.get('/main', (req, res, next) => {
     let coffeeLocation = coffee.map (coffee => {
       return coffee.location.coordinates
     })
+    let coffeeName = coffee.map (coffee => {
+      return coffee.name
+    })
+    let coffeeId = coffee.map (coffee => {
+      return coffee._id
+    })
     console.log(coffeeLocation)
-    res.render('main', {mapboxAPIKey, coffeeLocation});
+    res.render('main', {mapboxAPIKey, coffeeLocation, coffeeName, coffeeId});
   })
   
 });
@@ -34,5 +48,43 @@ router.post('/main', (req, res, next)=> {
   let searchInput = req.body.search;
   res.render('message', {searchInput})
 })
+
+
+router.get('/cafe/:id', (req, res, next) => {
+let id = req.params.id
+
+Cafe.findById(id)
+// .populate("_creator")
+  .then (cafe => {
+    res.render("cafe", {cafe})
+})
+})
+
+router.get('/auth/cafe/:id/add-comment', ensureAuthenticated, (req, res, next) => {
+  let id = req.params.id
+
+
+Cafe.findById(id)
+  .then (cafe => {
+    res.render("add-comment", {cafe})
+})
+})
+
+
+router.post('/cafe/:id', (req, res, next)=> {
+  let id = req.params.id;
+  console.log("Test", req.body.comment)
+  let comment = req.body.comment
+  Cafe.findByIdAndUpdate(id, {comments: {
+    content: comment,
+    _creator: req.user._id,
+    createdAt: Date.now()
+  }
+})
+
+  .then (cafe => {
+    res.redirect('/cafe/'+id)
+  })
+});
 
 module.exports = router;
