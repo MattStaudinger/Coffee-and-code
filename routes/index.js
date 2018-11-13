eff5f7c55346154873f563e84574396f7d34ead2
Nele Luepkes <nluepkes@gmail.com>
2018-11-13 15:04:02 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,46 +3,14 @@ const nodemailer = require('nodemailer');
 const router  = express.Router();
 const uploadCloud = require('../config/cloudinary.js');
 const {google} = require('googleapis');
-const youtube = google.youtube({
-   version: 'v3',
-   auth: process.env.YOUTUBE_API_KEY
-});
-
-
-
-
-
-
 
 /* GET home page */
 router.get('/', (req, res, next) => {
         res.render('index');
       });
 
-router.get('/query', (req, res, next) => {
-  youtube.search.list({
-    maxResults: "5",
-      part: 'snippet',
-      q: 'Sharks',
-      type: "video",
-      videoEmbeddable: "true"
-    }, function (err, data) {
-      if (err) {
-        console.error('Error: ' + err);
-      }
-      if (data) {
-        console.log(data.data.items[0])
-        res.redirect('/');
-      }
-    });
-});
-
 router.get('/about', (req, res, next) => {
   res.render('about');
-});
-
-router.get('/video', (req, res, next) => {
-  res.render('video');
 });
 
 router.post('/send-email', (req, res, next)=> {
