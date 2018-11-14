16703880187c15955fe7112d8dd8cf9ba49c10b2
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-11-14 16:35:35 +0100

    New URI

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://heroku_hkx3p829:856dou9ic2vgv8vfirjrojbj6h@ds137863.mlab.com:37863/heroku_hkx3p829', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
